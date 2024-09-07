241374cb542e1346f0dc6011a2021c544380d550
Konatham <bharathreedy.konatham@conduent.com>
2024-09-07 17:29:49 -0400

    updating our directory page with directory components with category componenets

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './categories.styles.scss'
+const CategoryItem = ({category}) => {
+    const {title,imageUrl} = category
+    console.log(title,imageUrl)
+    return (
+        <div  className="category-container">
+            <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}></div>
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>Shop Now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
